8c2416f3d6074ec224bd80df9349ea6425c0a8df
Chandler Kenworthy <chandlerkenworthy@gmail.com>
2022-11-25 14:45:28 +0000

    Add abstract Cipher base class

---
+++ src/MPAGSCipher/Cipher.hpp
@@ -0,0 +1,23 @@
+#ifndef MPAGS_CIPHER_CIPHER_HPP
+#define MPAGS_CIPHER_CIPHER_HPP
+#include "CipherMode.hpp"
+#include <string>
+
+/**
+ * \file Cipher.hpp
+ * \brief Contains the purely abstract Cipher base class
+*/
+class Cipher {
+    public:
+        Cipher() = default;
+        Cipher(const Cipher& rhs) = default;
+        Cipher(Cipher&& rhs) = default;
+        Cipher& operator=(const Cipher& rhs) = default;
+        Cipher& operator=(Cipher&& rhs) = default;
+        virtual ~Cipher() = default;
+        virtual std::string applyCipher(std::string& inputText, const CipherMode cipherMode) const = 0;
+        // Equals zero used to tell the compiler that the function is pure virtual i.e. no implementation
+        // is provided for the method
+};
+
+#endif
