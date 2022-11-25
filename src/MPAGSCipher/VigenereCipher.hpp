0e21e7edb68e27e76c2b0a042d691afc6bd23783
Chandler Kenworthy <chandlerkenworthy@gmail.com>
2022-11-25 10:32:19 +0000

    Add documentation to VigenereCipher

--- src/MPAGSCipher/VigenereCipher.hpp
+++ src/MPAGSCipher/VigenereCipher.hpp
@@ -3,12 +3,41 @@
 #include "CipherMode.hpp"
 #include <string>
 
+/**
+ * \file VigenereCipher.hpp
+ * \brief Contains declaration for VigenereCipher class
+*/
+
+/**
+ * \class VigenereCipher
+ * \brief Encrypt or decrypt text using the Vigenere cipher with the given key
+ */
 class VigenereCipher {
     public:
-        VigenereCipher(const std::string& key);
+        /**
+         * \brief Create a new VigenereCipher with the given key
+         * 
+         * \param key the key to use in the cipher
+        */
+        explicit VigenereCipher(const std::string& key);
+
+        /**
+         * \brief Set the key to be used for encryption/decryption
+         * 
+         * \param key the key to use in the cipher
+        */
         void setKey(const std::string& key);
+
+        /**
+         * \brief Apply the cipher to the provided text
+         * 
+         * \param inputText the text to encrypt/decrypt
+         * \param cipherMode whether to encrypt or decrypt the text
+         * \return the result of applying the cipher to the input text
+        */
         std::string applyCipher(const std::string& inputText, const CipherMode /*cipherMode*/) const;
     private:
+        // The cipher key
         std::string key_;
 };
 
